082ac55d16b1ea12a091f1aa3705941926caf011
Krishna8773 <patilkrishna800707@gmail.com>
2025-11-17 18:56:10 +0530

    Update server to use environment port variable

--- app.js
+++ app.js
@@ -107,7 +107,9 @@ app.use((err,req,res,next)=>{
 });
 
 
+const port = process.env.PORT || 8080;
 
-app.listen("8080",()=>{
-    console.log("App is Listening on port 8080");
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
+
