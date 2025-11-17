194107b212cb2066670979f0c378635254d5aa77
Krishna8773 <patilkrishna800707@gmail.com>
2025-11-17 17:56:11 +0530

    Refactor MongoDB connection setup
    Updated MongoDB connection URL to use environment variable and added connection options.

--- init/index.js
+++ init/index.js
@@ -2,24 +2,17 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const MONGO_URL = process.env.MONGO_URL;
 
-main().then(() =>{
-    console.log("connected to DB");
-}).catch(err =>{
-    console.log(err);
-})
 async function main() {
-    await mongoose.connect(MONGO_URL);
-};
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        tls: true,
+    });
+    console.log("Connected to DB");
+}
 
-const initDB = async () =>{
-   await Listing.deleteMany({});
-   initData.data=initData.data.map((obj)=>({
-    ...obj,
-    owner:'689632055f49a47dca0e4c60'
-   }));
-   await Listing.insertMany(initData.data);
-   console.log("data Was initialized ");
-};
-initDB();
+main().catch(err => {
+    console.log("MongoDB connection error:", err);
+});
